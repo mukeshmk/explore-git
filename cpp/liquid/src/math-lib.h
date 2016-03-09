f6ef351927a74e661df224cfb518b3ece09029b9
mukeshmk <mukeshbaji007@gmail.com>
2016-03-10 02:17:44 +0530

    Modified the math-lib.h header file
    Added the file links to the math-lib.h header file
    Changed the function names in ml_combos and ml_ perms

--- cpp/liquid/src/math-lib.h
+++ cpp/liquid/src/math-lib.h
@@ -10,5 +10,12 @@ double ml_div(double, double);
 double ml_sub(double, double);
 double ml_power(double, double);
 double ml_mod(double,double);
+double ml_combos(double,double);
+double ml_factorise(double);
+double ml_gcd(double,double);
+double ml_lcm(double,double);
+double ml_isPrime(double);
+double ml_nextPrime(double);
+double ml_perms(double,double);
 
 #endif
--- cpp/liquid/src/ml_perms.c
+++ cpp/liquid/src/ml_perms.c
@@ -1,6 +1,6 @@
 #include "math-lib.h"
 
-double ml_power(double n1, double n2){
+double ml_perms(double n1, double n2){
     // convert these to ints!
     // returning a double isn't really wrong...
     return 0;
